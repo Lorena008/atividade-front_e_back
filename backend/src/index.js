94603f4040a31d2514cb5b0d8dff5ace1fe7f477
Lorena Mendes Gomes <lorenamendesgomes2009@gmail.com>
2025-10-20 17:22:00 -0300

    Finalização do método POST

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { request, response } from "express"
 import cors from "cors"
 
 import { persons } from "./persons.js"
@@ -9,9 +9,20 @@ const port = 3333
 // GET, POST, PATCH, PUT, DELETE
 
 app.use(cors())
+app.use(express.json())
 
 app.get("/", (request, response) => {
     response.json(persons)
+})
+
+app.post("/cadastrar", (request, response) => {
+    // desestruturação
+    const { user } = request.body
+    console.log(user)
+
+    // cadastro no banco de dados
+
+    response.status(201).json({ message: "Usuário cadastrado com sucesso!" })
 })
 
 app.listen(port, () => {
